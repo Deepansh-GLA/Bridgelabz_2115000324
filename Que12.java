eaceff695167ef61737039eca3bbc9b18814fbb0
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-01-22 22:45:57 +0530

    16 questions

---
+++ Que12.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Que12{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		double ht=sc.nextDouble();
+		double base=sc.nextDouble();
+		System.out.println("The area of Triangle is"+(double)(base*ht)/2);
+	}
+}
---
+++ Que13.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Que13{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		double p=sc.nextDouble();
+		System.out.println("The length of the side is"+(double)p/4+" whose perimeter is "+p);
+	}
+}
